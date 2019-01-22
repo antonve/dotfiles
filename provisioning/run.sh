dde4eca582096a7587d2b1fe14a75bb88a82b500
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2019-01-22 18:12:41 +0900

    introduce secrets for account password

--- provisioning/run.sh
+++ provisioning/run.sh
@@ -10,8 +10,14 @@ for a in "$@"; do
   ARGS=(${ARGS[@]} "$a")
 done
 
+if ! [ -f ./secrets.yml ]; then
+  cp ./secrets{.sample,}.yml
+  [ "${DOTFILES_NOEDIT_SECRETS:-0}" -eq 0 ] && vim ./secrets.yml
+fi
+
 ${CMD[@]} \
   -i 'localhost,' \
   --extra-vars='@config.yml' \
+  --extra-vars='@secrets.yml' \
   ${ARGS[@]} \
   playbook.yml
