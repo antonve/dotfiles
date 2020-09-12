d0bc5a52b787313d26eabd0a8925f74bfe997141
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2020-09-12 18:05:39 +0900

    tweak zsh

--- zsh/zshrc.d/base/shell.zsh
+++ zsh/zshrc.d/base/shell.zsh
@@ -1,3 +1,4 @@
+# PROMPT
 autoload -U promptinit; promptinit
 prompt pure
 
@@ -6,3 +7,9 @@ zstyle :prompt:pure:git:stash show yes
 
 # Set color for path in prompt
 zstyle :prompt:pure:path color blue
+
+# Don't error on comments
+setopt interactivecomments
+
+# Automatically push the directory to the directory stack when using `cd`
+setopt auto_pushd
